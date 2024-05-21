dec43dedef2db661eca70012679ab2e084f4f40d
mtbyk <mtbyk@mtbyk-air.local>
2024-05-22 01:52:24 +0900

    スクリプト追加

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#
+# 各種スクリプトを呼び出す
+./setupApps.sh
+./makeSymLn.sh
---
+++ setupApps.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Oh My Zshの導入
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# zsh-autosuggestionsの導入
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# autojumpとfzfのインストール
+if command -v apt-get >/dev/null 2>&1; then
+    sudo apt-get update
+    sudo apt-get install -y autojump fzf
+elif command -v brew >/dev/null 2>&1; then
+    brew update
+    brew install autojump fzf
+else
+    echo "apt-getもbrewも見つかりませんでした。手動でautojumpとfzfをインストールしてください。"
+fi
+
+# snapがあってdockerがなければsnapでdockerをインストール
+if command -v snap >/dev/null 2>&1; then
+    if ! command -v docker >/dev/null 2>&1; then
+        sudo snap install docker
+    fi
+fi
+
+# snapのパスをzshに追加
+echo 'export PATH=$PATH:/snap/bin' >> ~/.zshrc
+
+# gitのデフォルトエディタをnvimに設定
+git config --global core.editor "nvim"
+

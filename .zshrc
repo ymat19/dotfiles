62ba73849a0c6ca01eec747a2b6172081c9ab50c
ymat <you@example.com>
2024-05-29 13:07:26 +0900

    PATHの通し方が違った

--- .zshrc
+++ .zshrc
@@ -11,6 +11,7 @@ fi
 # Path to your oh-my-zsh installation.
 export ZSH="$HOME/.oh-my-zsh"
 export PATH=$PATH:/snap/bin
+export PATH="$PATH:/opt/nvim-linux64/bin"
 
 # Set name of the theme to load --- if set to "random", it will
 # load a random theme each time oh-my-zsh is loaded, in which case,
--- setupApps.sh
+++ setupApps.sh
@@ -31,7 +31,6 @@ fi
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux64.tar.gz
-export PATH="$PATH:/opt/nvim-linux64/bin"
 
 # snapがあってdockerがなければsnapでdockerをインストール
 if command -v snap >/dev/null 2>&1; then

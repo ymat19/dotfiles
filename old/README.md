6dc187b321ba8a84d7632faeb097cc835f350e6d
ymat19 <>
2025-02-11 18:01:46 +0900

    remove olds

--- old/README.md
+++
@@ -1,5 +0,0 @@
-# 設定置き場
-## メモ
-- sudo -E で実行する
-- Dockerの中でカラー表示`-e "TERM=xterm-256color"`
-- 履歴が正しく保存されない場合はzsh_historyの所有者を確認
--- old/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# 各種スクリプトを呼び出す
-apt-get update && apt-get install -y zsh
-chsh -s $(which zsh)
-./setupApps.sh
-./makeSymLn.sh
-

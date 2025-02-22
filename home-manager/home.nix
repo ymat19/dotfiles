d7cfe1489c7fe70d72472593688bb3f4e06f80ff
ymat19 <ymat19@example.com>
2025-02-23 00:18:01 +0900

    add gh

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -32,6 +32,7 @@ in
     # # "Hello, world!" when run.
     hello
     gcc
+    gh
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the

bed0d7ba6b4b2f1d224d7cc9ef50273b068398d9
ymat19 <ymat19@example.com>
2025-02-28 04:13:09 +0900

    add wget

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -33,6 +33,7 @@ in
     hello
     gcc
     gh
+    wget
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the

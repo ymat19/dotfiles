d879424eb0ba7592d82831c67dafd02f1c3940ce
ymat19 <ymat19@example.com>
2025-03-04 07:50:49 +0900

    add lazydocker

--- home-manager/modules/tui-tools.nix
+++ home-manager/modules/tui-tools.nix
@@ -5,6 +5,10 @@
     enable = true;
   };
 
+  home.packages = lib.mkAfter (with pkgs; [
+    lazydocker
+  ]);
+
   programs.zsh.initExtra = lib.mkAfter ''
       # https://github.com/jesseduffield/lazygit/issues/1330#issuecomment-983826789
       # checks to see if we are in a windows or linux dir
@@ -14,7 +18,7 @@
           *) return $(false);;
         esac
       }
-      # wrap the lazygit command to either run windows lazygit or Linux lazygit
+
       function lazygit {
         if isWinDir
         then
@@ -23,10 +27,20 @@
           command lazygit "$@"
         fi
       }
+
+      function lazydocker {
+        if isWinDir
+        then
+          lazydocker.exe "$@"
+        else
+          command lazydocker "$@"
+        fi
+      }
   '';
 
   programs.tmux.extraConfig = lib.mkAfter ''
       # https://www.m3tech.blog/entry/dotfiles-bonsai#Tmux%E7%B7%A8
-      bind g popup -d '#{pane_current_path}' -w90% -h90% -E zsh -c "source ~/.zshrc && lazygit" # (prefix) gでlazygitを起動する
+      bind g popup -d '#{pane_current_path}' -w90% -h90% -E zsh -c "source ~/.zshrc && lazygit"
+      bind c popup -d '#{pane_current_path}' -w90% -h90% -E zsh -c "source ~/.zshrc && lazydocker"
   '';
 }

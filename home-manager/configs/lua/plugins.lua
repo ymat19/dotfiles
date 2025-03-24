bae576a9e8dba90f3ce038eb8c0c64c8cd57656b
ymat19 <ymat19>
2025-03-25 05:57:08 +0900

    refactor: split lua

---
+++ home-manager/configs/lua/plugins.lua
@@ -0,0 +1,48 @@
+-- plugins setup
+require'substitute'.setup{}
+require'lspconfig'.nixd.setup{}
+require("hardtime").setup()
+require('neoscroll').setup({ mappings = {'<C-u>', '<C-d>'} })
+
+-- avoid confilict: surround, leap
+require'nvim-surround'.setup{}
+vim.api.nvim_del_keymap('v', 'S')
+require('leap').create_default_mappings()
+
+-- treesitter
+require'nvim-treesitter.configs'.setup {
+  highlight = {
+    enable = true,
+  },
+  indent = {
+    enable = true,
+  },
+}
+
+require("oil").setup()
+vim.keymap.set("n", "-", "<CMD>vs<CR><CMD>Oil<CR>", { desc = "Open parent directory" })
+vim.keymap.set("n", "_", "<CMD>vnew<CR><CMD>Oil<CR>", { desc = "Open parent directory" })
+
+vim.cmd[[colorscheme tokyonight]]
+
+-- quick scope https://zenn.dev/neo/scraps/49266fed7ce6b6
+vim.cmd[[highlight QuickScopePrimary guifg='#afff5f' gui=underline ctermfg=155 cterm=underline]]
+vim.cmd[[highlight QuickScopeSecondary guifg='#5fffff' gui=underline ctermfg=81 cterm=underline]]
+
+-- substitute-nvim
+vim.keymap.set("n", "R", require('substitute').operator, { noremap = true })
+vim.keymap.set("n", "RR", require('substitute').line, { noremap = true })
+vim.keymap.set("x", "R", require('substitute').visual, { noremap = true })
+
+-- vim-expand-region
+vim.keymap.set('n', 'K', '<Plug>(expand_region_expand)', { noremap = false, silent = true })
+vim.keymap.set('x', 'K', '<Plug>(expand_region_expand)', { noremap = false, silent = true })
+vim.keymap.set('x', 'J', '<Plug>(expand_region_shrink)', { noremap = false, silent = true })
+
+-- clever-f
+vim.g.clever_f_smart_case = 1
+vim.g.clever_f_fix_key_direction = 1
+vim.g.clever_f_chars_match_any_signs = ";"
+vim.keymap.set('n', ';', '<Plug>(clever-f-repeat-forward)', { noremap = false })
+vim.keymap.set('n', ',', '<Plug>(clever-f-repeat-back)', { noremap = false })
+

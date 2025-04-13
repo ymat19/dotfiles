6a7c6430ced4366c5d278f8de7be5da9c0c9af9f
ymat19 <ymat19@example.com>
2025-04-13 12:55:21 +0900

    feat: add hyprland keybind

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -11,8 +11,15 @@
     enable = true;
     settings = {
       bind = [
-        "SUPER, R, exec, wofi --show drun"
-        "SUPER, Q, exec, kitty"
+        "SUPER, D, exec, wofi --show drun"
+        "SUPER, Return, exec, kitty"
+        "SUPER, Q, killactive"
+        "SUPER, h, movefocus, l"
+        "SUPER, l, movefocus, r"
+        "SUPER, j, movefocus, d"
+        "SUPER, k, movefocus, u"
+        "SUPER, space, togglefloating"
+        "SUPER_SHIFT, E, exit"
       ];
     };
   };

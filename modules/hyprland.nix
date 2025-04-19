4b552c44fce2c0bae7c9d39e6a6cc57225e70639
ymat19 <ymat19@example.com>
2025-04-19 10:05:26 +0900

    feat: add waybar

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -18,17 +18,23 @@ then
       enable = true;
       extraConfig = builtins.readFile ../configs/hyprlock.conf;
     };
-    ## ─ optional ─  一定時間アイドルになったら自動で Hyprlock を呼ぶ
+
     services.hypridle = {
-      enable = true; # Hyprland 公式アイドaルデーモン :contentReference[oaicite:1]{index=1}
+      enable = true;
       settings = {
-        general = { after = 300; }; # 300 秒アイドルで "lock" シグナル
+        general = {
+          after = 300;
+        }; # 300 秒アイドルで "lock" シグナル
         listener = [
           { on = "lock"; exec = "hyprlock"; } # 画面ロック
           { on = "after"; exec = "hyprctl dispatch dpms off"; } # さらに消灯
           { on = "resume"; exec = "hyprctl dispatch dpms on"; } # 復帰で点灯
         ];
       };
+    };
+
+    programs.waybar = {
+      enable = true;
     };
 
     i18n.inputMethod = {

65b88c672acc2cf9786dc947df6f4834fa1168f0
ymat19 <ymat19@example.com>
2025-04-20 22:01:23 +0900

    feat: remove waybar

--- modules/nixos/hyprland.nix
+++ modules/nixos/hyprland.nix
@@ -33,10 +33,6 @@
     };
   };
 
-  programs.waybar = {
-    enable = true;
-  };
-
   i18n.inputMethod = {
     enabled = "fcitx5";
     fcitx5.addons = with pkgs; [ fcitx5-mozc fcitx5-configtool ];

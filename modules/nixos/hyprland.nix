955d32065bd7c80dd2ceb4c862bd886ecac0b88b
ymat19 <ymat19@example.com>
2025-05-09 04:53:06 +0900

    feat: kitty font size

--- modules/nixos/hyprland.nix
+++ modules/nixos/hyprland.nix
@@ -9,6 +9,10 @@
 
   programs.kitty = {
     enable = true;
+    font = {
+      name = "";
+      size = 14;
+    };
     settings = {
       background_opacity = 0.8;
       confirm_os_window_close = "0";

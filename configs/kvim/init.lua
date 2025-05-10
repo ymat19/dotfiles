c72ac23abd616f88784d918ed81152e74e7ca6a6
ymat19 <ymat19@example.com>
2025-05-11 03:37:27 +0900

    feat: nvim transparent

--- configs/kvim/init.lua
+++ configs/kvim/init.lua
@@ -696,8 +696,11 @@ require('lazy').setup({
     config = function()
       ---@diagnostic disable-next-line: missing-fields
       require('tokyonight').setup {
+        transparent = true,
         styles = {
           comments = { italic = false }, -- Disable italics in comments
+          sidebars = "transparent",
+          floats = "transparent",
         },
       }
 

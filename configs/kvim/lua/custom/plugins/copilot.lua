fb18ca20ad4f2f04105ee3a5fa63ef6238797b0e
ymat19 <ymat19@example.com>
2025-05-24 08:27:39 +0900

    feat: add avante nvim

--- configs/kvim/lua/custom/plugins/copilot.lua
+++ configs/kvim/lua/custom/plugins/copilot.lua
@@ -1,4 +1,5 @@
 return {
+
   {
     'zbirenbaum/copilot.lua',
     event = 'VeryLazy',
@@ -20,82 +21,54 @@ return {
     end,
   },
   {
-    'CopilotC-Nvim/CopilotChat.nvim',
+    'yetone/avante.nvim',
     event = 'VeryLazy',
-    dependencies = {
-      { 'zbirenbaum/copilot.lua' },
-      { 'nvim-lua/plenary.nvim', branch = 'master' }, -- for curl, log and async functions
-    },
-    build = 'make tiktoken', -- Only on MacOS or Linux
+    version = false, -- Never set this value to "*"! Never!
     opts = {
-      -- sticky = { '日本語で話してください' },
-      show_help = 'yes',
+      -- add any opts here
+      -- for example
+      provider = 'copilot',
     },
-    keys = {
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    build = 'make',
+    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'stevearc/dressing.nvim',
+      'nvim-lua/plenary.nvim',
+      'MunifTanjim/nui.nvim',
+      --- The below dependencies are optional,
+      'echasnovski/mini.pick', -- for file_selector provider mini.pick
+      'nvim-telescope/telescope.nvim', -- for file_selector provider telescope
+      'hrsh7th/nvim-cmp', -- autocompletion for avante commands and mentions
+      'ibhagwan/fzf-lua', -- for file_selector provider fzf
+      'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
+      'zbirenbaum/copilot.lua', -- for providers='copilot'
       {
-        '<leader>ac',
-        function()
-          vim.cmd 'CopilotChatOpen'
-        end,
-        desc = 'OPEN CHAT',
-      },
-    },
-    config = function()
-      require('CopilotChat').setup {
-        -- https://qiita.com/haw_ohnuma/items/1ec8ef5091b440cbb8bd
-        prompts = {
-          Explain = {
-            prompt = '/COPILOT_EXPLAIN コードを日本語で説明してください',
-            mapping = '<leader>ae',
-            description = 'EXPLAIN',
-          },
-          Review = {
-            prompt = '/COPILOT_REVIEW コードを日本語でレビューしてください。',
-            mapping = '<leader>ar',
-            description = 'REVIEW',
-          },
-          Fix = {
-            prompt = '/COPILOT_FIX このコードには問題があります。バグを修正したコードを表示してください。説明は日本語でお願いします。',
-            mapping = '<leader>af',
-            description = 'FIX',
-          },
-          Optimize = {
-            prompt = '/COPILOT_REFACTOR 選択したコードを最適化し、パフォーマンスと可読性を向上させてください。説明は日本語でお願いします。',
-            mapping = '<leader>ao',
-            description = 'REFACTOR',
-          },
-          Docs = {
-            prompt = '/COPILOT_GENERATE 選択したコードに関するドキュメントコメントを日本語で生成してください。',
-            mapping = '<leader>ad',
-            description = 'GENERATE',
-          },
-          Tests = {
-            prompt = '/COPILOT_TESTS 選択したコードの詳細なユニットテストを書いてください。説明は日本語でお願いします。',
-            mapping = '<leader>at',
-            description = 'TESTS',
-          },
-          FixDiagnostic = {
-            prompt = 'コードの診断結果に従って問題を修正してください。修正内容の説明は日本語でお願いします。',
-            mapping = '<leader>ad',
-            description = 'DIAGNOSTICS',
-            selection = require('CopilotChat.select').diagnostics,
-          },
-          Commit = {
-            prompt = '実装差分に対するコミットメッセージを日本語で記述してください。',
-            mapping = '<leader>am',
-            description = 'MESSAGE DIFF',
-            selection = require('CopilotChat.select').gitdiff,
-          },
-          CommitStaged = {
-            prompt = 'ステージ済みの変更に対するコミットメッセージを日本語で記述してください。',
-            mapping = '<leader>as',
-            description = 'MESSAGE STAGING',
-            selection = function(source)
-              return require('CopilotChat.select').gitdiff(source, true)
-            end,
+        -- support for image pasting
+        'HakonHarnes/img-clip.nvim',
+        event = 'VeryLazy',
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
           },
         },
-      }
-    end,
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { 'markdown', 'Avante' },
+        },
+        ft = { 'markdown', 'Avante' },
+      },
+    },
   },
 }

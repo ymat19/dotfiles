6dca18d09f133bea4d755dd69979d360b0405c24
ymat19 <ymat19@example.com>
2025-05-27 10:07:32 +0900

    feat: add git-lfs

--- modules/git-tools.nix
+++ modules/git-tools.nix
@@ -10,19 +10,20 @@
 
   home.packages = lib.mkAfter (with pkgs; [
     ghq
+    git-lfs
   ]);
 
   programs.zsh.initExtra = lib.mkAfter ''
-      # https://qiita.com/tomoyamachi/items/e51d2906a5bb24cf1684
-      function ghq-fzf() {
-        local src=$(ghq list -p | fzf --preview "bat --color=always --style=header,grid --line-range :80 {}/README.*")
-        if [ -n "$src" ]; then
-          BUFFER="cd $src"
-          zle accept-line
-        fi
-        zle -R -c
-      }
-      zle -N ghq-fzf
-      bindkey '^]' ghq-fzf
+    # https://qiita.com/tomoyamachi/items/e51d2906a5bb24cf1684
+    function ghq-fzf() {
+      local src=$(ghq list -p | fzf --preview "bat --color=always --style=header,grid --line-range :80 {}/README.*")
+      if [ -n "$src" ]; then
+        BUFFER="cd $src"
+        zle accept-line
+      fi
+      zle -R -c
+    }
+    zle -N ghq-fzf
+    bindkey '^]' ghq-fzf
   '';
 }

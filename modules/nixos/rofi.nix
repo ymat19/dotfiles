6eaaeb3711fde4eb504c53b195b9ee3fcf8e326a
ymat19 <ymat19@example.com>
2025-09-29 07:02:27 +0900

    chore: update hypt r config

--- modules/nixos/rofi.nix
+++ modules/nixos/rofi.nix
@@ -3,7 +3,7 @@
   # https://zenn.dev/watagame/articles/hyprland-nix#launcher
   programs.rofi = {
     enable = true;
-    package = pkgs.rofi-wayland;
+    package = pkgs.rofi;
     #	Refered: https://github.com/NeshHari/XMonad/blob/main/rofi/.config/rofi/config.rasi
     theme = ../../configs/rofi.rasl;
   };

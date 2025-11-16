6b0bebabf2b4bb756aed1cbecfca91f03eac2766
ymat19 <ymat19@example.com>
2025-11-17 04:50:05 +0900

    chore: update nvim theme

--- configs/kvim/lua/custom/plugins/bufferline.lua
+++ configs/kvim/lua/custom/plugins/bufferline.lua
@@ -3,6 +3,12 @@ return {
   version = '*',
   dependencies = 'nvim-tree/nvim-web-devicons',
   config = function()
-    require('bufferline').setup {}
+    require('bufferline').setup {
+      highlights = {
+        fill = {
+          bg = 'NONE',
+        },
+      },
+    }
   end,
 }

d736198fd05b558bf20377d79afeaa3704800f59
ymat19 <ymat19@example.com>
2025-11-17 00:28:49 +0900

    feat: add tmux theme

--- modules/tmux.nix
+++ modules/tmux.nix
@@ -10,5 +10,23 @@
     customPaneNavigationAndResize = true;
     shell = "${pkgs.zsh}/bin/zsh";
     extraConfig = builtins.readFile ../configs/tmux.conf;
+    plugins = with pkgs.tmuxPlugins; [
+      #tmux-resurrect
+      #tmux-continuum
+      #tmux-sensible
+      #tmux-prefix-highlight
+      #tmux-copycat
+      sensible
+      logging
+      {
+        plugin = tokyo-night-tmux;
+        extraConfig = ''
+### Tokyo Night Theme configuration
+set -g @tokyo-night-tmux_show_hostname 1
+set -g @tokyo-night-tmux_transparent 1
+        '';
+      }
+    ];
   };
+
 }
--- nixos-configurations/baremetal.nix
+++ nixos-configurations/baremetal.nix
@@ -76,13 +76,14 @@
       noto-fonts-cjk-sans
       noto-fonts-color-emoji
       nerd-fonts.fira-code
+      unifont_upper
     ];
     fontDir.enable = true;
     fontconfig = {
       defaultFonts = {
         serif = [ "Noto Serif CJK JP" "Noto Color Emoji" ];
         sansSerif = [ "Noto Sans CJK JP" "Noto Color Emoji" ];
-        monospace = [ "JetBrainsMono Nerd Font" "Noto Color Emoji" ];
+        monospace = [ "FiraCode Nerd Font" "Unifont Upper" "Noto Color Emoji" ];
         emoji = [ "Noto Color Emoji" ];
       };
     };

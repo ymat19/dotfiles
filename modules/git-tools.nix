7222d5efa552b0c9df49d89a7542a3c1d6ac3488
ymat19 <ymat19@example.com>
2026-01-13 19:39:11 +0900

    feat: add gwq

--- modules/git-tools.nix
+++ modules/git-tools.nix
@@ -1,5 +1,23 @@
 { config, pkgs, lib, ... }:
 
+let
+  gwq = pkgs.buildGoModule rec {
+    pname = "gwq";
+    version = "unstable-2025-01-10";
+    src = pkgs.fetchFromGitHub {
+      owner = "d-kuro";
+      repo = "gwq";
+      rev = "c33ab3e9935df70c8a0a9eb1932ed9b24f5ab877";
+      hash = "sha256-Sl4oBvyjbM1rPPgSe3xf/PDF2whLzyBY9a9h2zsTNaU=";
+    };
+    vendorHash = "sha256-c1vq9yETUYfY2BoXSEmRZj/Ceetu0NkIoVCM3wYy5iY=";
+    subPackages = [ "cmd/gwq" ];
+    meta = {
+      description = "Git Worktree Manager";
+      homepage = "https://github.com/d-kuro/gwq";
+    };
+  };
+in
 {
   programs.git = {
     enable = true;
@@ -9,6 +27,9 @@
         name = "ymat19";
         email = "ymat19@example.com";
       };
+      ghq = {
+        root = "/home/ymat19/repos";
+      };
     };
   };
 
@@ -17,11 +38,13 @@
     enableGitIntegration = true;
   };
 
-  home.packages = lib.mkAfter (with pkgs; [
+  home.packages = lib.mkAfter ([
+    gwq
+  ] ++ (with pkgs; [
     ghq
     git-lfs
     act
-  ]);
+  ]));
 
   programs.zsh.initContent = lib.mkAfter ''
     # https://qiita.com/tomoyamachi/items/e51d2906a5bb24cf1684
@@ -35,5 +58,18 @@
     }
     zle -N ghq-fzf
     bindkey '^]' ghq-fzf
+
+    # gwq worktree selection
+    function gwq-fzf() {
+      local wt=$(gwq list -g | fzf --preview "ls -la {2}")
+      if [ -n "$wt" ]; then
+        local path=$(echo "$wt" | awk '{print $2}')
+        BUFFER="cd $path"
+        zle accept-line
+      fi
+      zle -R -c
+    }
+    zle -N gwq-fzf
+    bindkey '^o' gwq-fzf
   '';
 }
